283b9448c7e057540d656579f6d84c2d3735bb3f
anytarsier67 <42589471+anytarseir67@users.noreply.github.com>
2021-08-29 17:46:00 -0400

    initial commit

---
+++ robloxpy/__init__.py
@@ -0,0 +1,41 @@
+import asyncio
+from . import group
+from . import user
+from . import Utils
+from . import errors
+from . import asset
+from . import game
+import aiohttp
+import sys
+
+__version__ = "1.0.0.a1"
+
+this = sys.modules[__name__]
+
+this.CurrentCookie = None
+this.RawCookie = None
+
+async def get_session():
+    if this.session == None:
+        this.session = aiohttp.ClientSession()
+    return this.session
+
+this.session = None
+
+async def SetCookie(Cookie: str) -> None:
+    """
+    Set the current cookie for internal commands.
+    """
+    try:
+        CurrentCookie = {'.ROBLOSECURITY': Cookie}
+        session = aiohttp.ClientSession(cookies=CurrentCookie)
+        async with session.post('https://auth.roblox.com/') as Header:
+            session.headers['x-csrf-token'] = Header.headers['x-csrf-token']
+        session.headers["Origin"] = "https://www.roblox.com"
+        session.headers["Referer"] = "https://www.roblox.com/"
+    except:
+        raise errors.InvalidCookie()
+    if Utils.CheckCookie(Cookie) == False:
+        raise errors.InvalidCookie()
+    this.CurrentCookie = session
+    this.RawCookie = Cookie
