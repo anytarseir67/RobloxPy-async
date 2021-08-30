14a0a7aefc890ecab7ee4c0ffcc337192b14672b
anytarsier67 <42589471+anytarseir67@users.noreply.github.com>
2021-08-30 04:02:36 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
 from setuptools import setup
+import pathlib
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
 
 with open('requirements.txt') as f:
   requirements = f.read().splitlines()
@@ -6,9 +9,12 @@ with open('requirements.txt') as f:
 setup(name='robloxpy-async',
     version='1.0.0.a1',
     description='robloxpy-async is an async/await object oriented roblox API wrapper for python',
+    long_description=README,
+    long_description_content_type="text/markdown",
     author='anytarseir67 & KristanSmout',
     author_email = '',
     url='https://github.com/anytarseir67/RobloxPy-async',
+    license="GPLv3",
     packages=['robloxpy_async'],
     install_requires=requirements,
     )
