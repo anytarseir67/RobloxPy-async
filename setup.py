20f72b513ae00b8673064a61ffeec05d327cef94
anytarsier67 <42589471+anytarseir67@users.noreply.github.com>
2021-08-30 04:21:06 -0400

    version bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('requirements.txt') as f:
   requirements = f.read().splitlines()
 
 setup(name='robloxpy-async',
-    version='1.0.0.a1',
+    version='1.0.0',
     description='robloxpy-async is an async/await object oriented roblox API wrapper for python',
     long_description=README,
     long_description_content_type="text/markdown",
