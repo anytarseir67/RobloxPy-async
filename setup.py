868c173513acb36386aeb203a471e4548f8cc84a
anytarsier67 <42589471+anytarseir67@users.noreply.github.com>
2021-08-30 03:47:41 -0400

    change folder name, and name in setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 with open('requirements.txt') as f:
   requirements = f.read().splitlines()
 
-setup(name='robloxpy',
+setup(name='robloxpy-async',
     version='1.0.0.a1',
-    description='RobloxPy is an object oriented python wrapper for the roblox API.',
-    author='KristanSmout & anytarseir67',
+    description='robloxpy-async is an async/await object oriented roblox API wrapper for python',
+    author='anytarseir67 & KristanSmout',
     author_email = '',
-    url='https://github.com/anytarseir67/RobloxPyOfficial/tree/1.0/',
-    packages=['robloxpy'],
+    url='https://github.com/anytarseir67/RobloxPy-async',
+    packages=['robloxpy_async'],
     install_requires=requirements,
     )
